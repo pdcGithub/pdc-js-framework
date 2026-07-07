b073b910b1c3fac75101141965ffaa22927b6d48
Iron Man <pangdongcan@live.com>
2026-07-07 17:04:02 +0800

    dev: add third-party tool Bootstrap 5.

---
+++ tests/thirdpartys/bootstrap5/testBootstrap5.js
@@ -0,0 +1,23 @@
+/**
+ * In my license, all codes can be shared free of charge. 
+ * However, If my code is taken for commercial use, please maintain it yourself. 
+ * I am not obligated to take responsibility for your business application.
+ * Here is my email "pangdongcan@live.com"
+ * 
+ * Copyright © 2026 Micheal Pang. All rights reserved.
+ * 
+ * @file This file "testBootstrap5.js" is part of project "pdc-js-framework" , which is belong to Michael Pang (It's Me).
+ * @module xxxxxx
+ * @author  Micheal Pang (Dongcan Pang)
+ * @since  2026-07-06
+ * @description  这里是测试 Bootstrap 5 的处理。
+ */
+// "use strict"; // 这是严格模式下的 Javascript 代码
+
+function btn1Click(){
+    alert('测试...');
+}
+
+function btn2Click(){
+    alert('测试...2');
+}
