d00eab4affad709dba8b12be63f4d85d892cbbe8
Iron Man <pangdongcan@live.com>
2026-07-17 14:53:59 +0800

    dev: develop the module uiComponents/static.js and related unit tests.

---
+++ tests/uiComponents/testStatic.js
@@ -0,0 +1,42 @@
+/**
+ * In my license, all codes can be shared free of charge. 
+ * However, If my code is taken for commercial use, please maintain it yourself. 
+ * I am not obligated to take responsibility for your business application.
+ * Here is my email "pangdongcan@live.com"
+ * 
+ * Copyright © 2026 Micheal Pang. All rights reserved.
+ * 
+ * @file This file "testStatic.js" is part of project "pdc-js-framework" , which is belong to Michael Pang (It's Me).
+ * @author Micheal Pang (Dongcan Pang)
+ * @since 2026-07-17
+ * @description 这里是关于 UI 组件的静态模块 的测试。这里是一个对外接口，所以测试函数直接取子模块就行了。
+ */
+"use strict"; // 这是严格模式下的 Javascript 代码
+
+// 导入 static 模块用于测试
+import { PROTECTED_GET_CONTENT, Bootstrap5Object } from "../../uiComponents/static.js";
+
+// 导入子模块的测试函数
+import {
+    testBootstrap5ObjectCreate, testBootstrap5ObjectAble, testBootstrap5ObjectGet, testBootstrap5ObjectAdd,
+    testBootstrap5ObjectRemove, testBootstrap5ObjectHas, testBootstrap5ObjectClear, testBootstrap5ObjectTo,
+    testBootstrap5ObjectProteced
+} from "./static/base/testBootstrap5Object.js";
+
+/**
+ * 再增加一个测试 static 模块能否正常调用的测试
+ */
+function testStaticModule(){
+
+    let t1 = new Bootstrap5Object();
+    let t2 = t1[PROTECTED_GET_CONTENT]();
+}
+
+// =========== 导出测试函数
+export {
+    testBootstrap5ObjectCreate, testBootstrap5ObjectAble, testBootstrap5ObjectGet, testBootstrap5ObjectAdd,
+    testBootstrap5ObjectRemove, testBootstrap5ObjectHas, testBootstrap5ObjectClear, testBootstrap5ObjectTo,
+    testBootstrap5ObjectProteced,
+    
+    testStaticModule
+}
---
+++ uiComponents/static.js
@@ -0,0 +1,24 @@
+/**
+ * In my license, all codes can be shared free of charge. 
+ * However, If my code is taken for commercial use, please maintain it yourself. 
+ * I am not obligated to take responsibility for your business application.
+ * Here is my email "pangdongcan@live.com"
+ * 
+ * Copyright © 2026 Micheal Pang. All rights reserved.
+ * 
+ * @file This file "static.js" is part of project "pdc-js-framework" , which is belong to Michael Pang (It's Me).
+ * @module uiComponents/static
+ * @author Micheal Pang (Dongcan Pang)
+ * @since 2026-07-17
+ * @description 这里是一个 UI 组件的静态模块。它的主要作用是提供一些用于绘制的静态类或者常量。
+ * 在使用时，直接调用 static.js 模块就行了。注意：请不要直接调用这个模块下面的子模块，因为它们可能修改路径。使用时，请统一导入本模块。
+ */
+"use strict"; // 这是严格模式下的 Javascript 代码
+
+// ========= 导入子模块的内容
+import { PROTECTED_GET_CONTENT, Bootstrap5Object } from "./static/base/Bootstrap5Object.js";
+
+// ========= 导出
+export{
+    PROTECTED_GET_CONTENT, Bootstrap5Object
+}
