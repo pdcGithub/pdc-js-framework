9f4165e18995abd0bece01b0bb00ce9d190fb25c
Iron Man <pangdongcan@live.com>
2026-08-13 17:47:44 +0800

    dev: do some experiments.

---
+++ experiments/queryTable/queryTable.js
@@ -0,0 +1,14 @@
+/**
+ * In my license, all codes can be shared free of charge. 
+ * However, If my code is taken for commercial use, please maintain it yourself. 
+ * I am not obligated to take responsibility for your business application.
+ * Here is my email "pangdongcan@live.com"
+ * 
+ * Copyright © 2026 Micheal Pang. All rights reserved.
+ * 
+ * @file This file "queryTable.js" is part of project "pdc-js-framework" , which is belong to Michael Pang (It's Me).
+ * @author Micheal Pang (Dongcan Pang)
+ * @since 2026-08-13
+ * @description 这里是关于查询列表的组件实验
+ */
+"use strict"; // 这是严格模式下的 Javascript 代码
